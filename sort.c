77350866b1813b0e0c171a4b0fe67836ef7728be
Kai Luo <L51218861@126.com>
2012-03-01 16:35:56 +0800

    modify quick sort method

--- sort.c
+++ sort.c
@@ -106,6 +106,14 @@ void exchange(int *a, int *b)
 
 int partition(int array[], int start, int end)
 {
+/***************************** 方法1 ***************************
+**  每一轮迭代开始时，middle = array[start], 对于i小于        **
+**  start，array[i]小于等于middle；对于i大于end，             **
+**  array[i]大于等于middle；终止时，start == end，此时，      **
+**  start左边小于等于middle,start右边大于等于middle，start    **
+**  处为middle                                                **
+****************************************************************
+
 	while(start < end)
 	{
 		//中间数在start位置
@@ -120,6 +128,91 @@ int partition(int array[], int start, int end)
 	}
 
 	return start;
+
+*****************************************************************/
+
+
+/***************************** 方法2 ****************************
+** 每一轮迭代开始时，middle = array[start],对于i <= start,     **
+** array[i] <= middle; 对于i > end, array[i] >= middle; 终止时 **
+** start == end, 此时，array[start] <= middle, 再交换middle和  **
+** array[i]即可                                                **
+****************************************************************/
+
+	int mid = start;
+
+	while(start < end)
+	{
+		while(start < end && array[end] >= array[mid])
+			end--;
+
+		while(start < end && array[start] <= array[mid])
+			start++;
+
+		exchange(&array[start], &array[end]);
+	}
+
+	exchange(&array[start], &array[mid]);
+
+	return start;
+
+/****************************************************************/
+
+
+/************************** Method 3 ****************************
+** 每次迭代时均满足以下条件：1）如果start <= k <= i,则array[k] **
+** <= middle; 2）如果i+1 <= k <= j-1，则array[k] > x; 3）如果  **
+** k == r,则array[k] = middle；终止时，k = end，此时将middle   **
+** 与array[i+1]交换即可                                        **
+*****************************************************************
+
+	int i, j;
+	i = start - 1;
+	middle = array[end];
+
+	for(j = start; j < end; j++)
+	{
+		if(array[j] <= middle)
+		{
+			i++;
+			exchange(&array[i], &array[j]);
+		}
+	}
+
+	exchange(&array[end], &array[i+1]);
+
+	return i+1;
+
+****************************************************************/
+
+
+/****************************** 方法4 ***************************
+** 每次迭代开始时，middle = array[end],对于k <= i,array[k] <=  **
+** middle; 对于k >= j, array[k] >= middle; 终止时，i>=j，此时  **
+** j左边的均小于等于middle，右边大于等于middle                 **
+*****************************************************************
+
+	int i = start - 1;
+	int j = end + 1; 
+	int middle = array[start];
+	
+	while(true)
+	{
+		do{
+			j--;
+		}while(array[j] >= middle)
+
+		do{
+			i++;
+		}while(array[i] >= middle)
+
+		if(i < j)
+			exchange(&array[i], &array[j]);
+		else
+			return j;
+	}
+
+****************************************************************/
 }
 
 void quick_sort(int array[], int start, int end)
@@ -177,6 +270,44 @@ void merge_sort(int array[], int start, int end)
 	}
 }
 
+void shell_sort(int array[], int length)
+{
+	int i, j, k, inc, key;
+	
+	//从length/2为初始跨度分组
+	for(inc = length/2; inc > 0; inc /= 2)
+	{
+		//跨度长度个分组
+		for(i = 0; i < inc; i++)
+		{
+			//为每个分组进行插入排序
+			for(j = i+inc ; j <= length; j += inc)
+			{
+				k = j - inc;
+				key = array[j];
+
+				while(k >= 0 && array[k] > key)
+				{
+					array[k+inc] = array[k];
+					k -= inc;	
+				}
+				array[k+inc] = key;	
+			}
+		}  
+	}
+	
+}
+
+void heap_sort(int array[], int length)
+{
+
+}
+
+void radix_sort(int array[], int length)
+{
+
+}
+
 void usage()
 {
 	printf("Usage : sort -a -n\n\
@@ -213,6 +344,12 @@ int main(int argc, char *argv[])
 			method = 3;
 		else if(!strcmp(argv[2], "-4"))
 			method = 4;
+		else if(!strcmp(argv[2], "-5"))
+			method = 5;
+		else if(!strcmp(argv[2], "-6"))
+			method = 6;
+		else if(!strcmp(argv[2], "-7"))
+			method = 7;
 		else
 			method = 1;	
 	}
@@ -240,6 +377,18 @@ int main(int argc, char *argv[])
 	case 4:
 		printf("quick sort:\n");
 		quick_sort(my_array, 0, length);
+		break;
+	case 5:
+		printf("shell sort:\n");
+		shell_sort(my_array, length);
+		break;
+	case 6:
+		printf("heap sort:\n");
+		heap_sort(my_array, length);
+		break;
+	case 7:
+		printf("radix sort:\n");
+		radix_sort(my_array, length);
 		break;
 	}
 
